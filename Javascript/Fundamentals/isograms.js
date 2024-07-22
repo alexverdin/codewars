5023b8625096107298b3f56aaf87ced62af9bbb3
Alejandro Navarro <navarrov.alex@gmail.com>
2024-07-22 15:45:07 -0600

    added Set() constructor explanation

--- Javascript/Fundamentals/isograms.js
+++ Javascript/Fundamentals/isograms.js
@@ -1,3 +1,18 @@
+/**
+ * 7 Kyu
+ * An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+ * Implement a function that determines whether a string that contains only letters is an isogram.
+ * Assume the empty string is an isogram. Ignore letter case.
+ 
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true
+"aba" --> false
+"moOse" --> false (ignore letter case)
+
+ */
+
+// My Version
 function isIsogram(str){
   str = str.toLowerCase();
   if(str === ''){
@@ -14,4 +29,13 @@ function isIsogram(str){
   }
 }
 
-console.log(isIsogram('isIsogram'));
+// Optimized Version 😱
+/**
+ * Explanation:
+ * "Set()" is an object that allow us to store UNIQUE VALUES only
+ * when you pass the string to the Set() constructor, it stores each character ONLY ONCE.
+ * So, if you compare the size of the original string and the "setted" string and they're the same, the string was an Isogram
+ */
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
